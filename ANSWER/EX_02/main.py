05fe18731ffc8117ca9e76081b37d230f3581f6e
cmt14139 <135939030+cmt14139@users.noreply.github.com>
2024-06-07 15:47:58 +0900

    Add files via upload

---
+++ ANSWER/EX_02/main.py
@@ -0,0 +1,15 @@
+def dict_fizzbuzz(num):
+    dict = {n : "" for n in range(1, num + 1)}
+
+    for i in range(1, num + 1):
+        if i % 15 == 0:
+            dict[i] = "fizzbuzz"
+        elif i % 3 == 0:
+            dict[i] = "fizz"
+        elif i % 5 == 0:
+            dict[i] = "buzz"
+    
+    return dict
+
+print(dict_fizzbuzz(30))
+
